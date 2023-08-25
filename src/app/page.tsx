181dd63de91669a522e166c06429bc807019f1b5
RiahiAchraf <riahiachraf1@gmail.com>
2023-08-25 10:01:47 +0100

    feat: display list of jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
+'use client';
+
+import process from 'process';
+import { useEffect } from 'react';
+
+import { useGetJobs } from '@/api/jobs';
+import { useCurrentList } from '@/stores/useCurrentList';
+
 export default function Home() {
-  return <h2 className='text-green-500'>HR FLOW</h2>;
+  const setCurrentList = useCurrentList((state) => state.setCurrentList);
+  const currentList = useCurrentList((state) => state.currentList);
+
+  const { isLoading, data } = useGetJobs({ board_keys: JSON.stringify([BOARD_KEY]) });
+
+  useEffect(() => {
+    if (setCurrentList) {
+      setCurrentList(data?.data?.jobs);
+    }
+  }, [data, setCurrentList]);
+
+  return (
+    <>
+      <ul>Hello World</ul>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>{currentList?.map((item) => <li key={item?.id}>{item?.name}</li>)}</ul>
+      )}
+    </>
+  );
 }
+
+const BOARD_KEY = process.env.NEXT_PUBLIC_BOARD_KEY;
