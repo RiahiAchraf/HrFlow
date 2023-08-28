86307a53c1a5c6c1c517276537a08902c1b9e7bd
RiahiAchraf <riahiachraf1@gmail.com>
2023-08-28 17:04:37 +0100

    fix: disable video recording option

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents() {
       // implement node event listeners here
     },
+  },
+  video: false,
+  env: {
+    API_HOST: process.env.NEXT_PUBLIC_API_URL,
   },
 });
