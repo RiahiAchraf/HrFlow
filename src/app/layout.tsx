b6f21b6810cd62d981171e223e939a87c953216f
RiahiAchraf <riahiachraf1@gmail.com>
2023-08-29 13:52:58 +0100

    fix: set  maximum scale to 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   viewport: {
     width: 'device-width',
     initialScale: 1,
-    // maximumScale: 1,
+    maximumScale: 1,
     viewportFit: 'cover',
   },
 };
